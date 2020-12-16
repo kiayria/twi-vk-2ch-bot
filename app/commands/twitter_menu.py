c6d2799c67c8d0d5ab742d4b0e317214f7c08944
Sibikin Konstantin <sibikin.kv@gmail.com>
2020-12-16 21:09:46 +0300

    .

--- app/commands/twitter_menu.py
+++ app/commands/twitter_menu.py
@@ -1,7 +1,8 @@
 from aiogram import types
 
-from app import dp, bot, Form, twitter_auth
+from app import dp, bot, twitter_auth
 from app.utils.keyboards import get_twi_markup
+from app.utils.states import TwitterForm
 
 
 @dp.callback_query_handler(text='twi_btn', state='*')
@@ -14,7 +15,7 @@ async def twi_menu(query, state):
     )
 
 
-@dp.callback_query_handler(text='twi_login', state=Form.twitter)
+@dp.callback_query_handler(text='twi_login', state='*')
 async def twi_login(query, state):
     await query.answer()
     # await query.message.edit_text(
@@ -24,8 +25,9 @@ async def twi_login(query, state):
     await bot.send_message(query.from_user.id, str(twitter_auth.get_authorization_url()))
 
 
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('twi_tweet'), state=Form.twitter)
+@dp.callback_query_handler(text='twi_tweet', state='*')
 async def twi_tweet(query, state):
+
     user_text = str(query.data).replace('twi_tweet', '')
     await query.answer(text=user_text)
     await query.message.edit_text(
@@ -34,11 +36,25 @@ async def twi_tweet(query, state):
     )
 
 
-@dp.message_handler(commands='twi_tweet', state=Form.twitter)
+@dp.message_handler(commands='twi_tweet', state='*')
 async def cmd_start(message, state):
     user_text = str(message.text).replace('twi_tweet', '')
     await message.answer(
         text=f'Sent: {user_text}',
         reply_markup=get_twi_markup()
     )
+
+
+@dp.message_handler(state=TwitterForm.tweet)
+async def process_tweet(message, state):
+    async with state.proxy() as data:
+        api = data['api']
+        api.update_status(str(message.text))
+
+        await message.answer(
+            text='Твит отправлен',
+            reply_markup=get_twi_markup()
+        )
+    await state.finish()
+
 
--- app/utils/states.py
+++ app/utils/states.py
@@ -1,6 +1,12 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
+class TwitterForm(StatesGroup):
+    tweet = State()
+    stream = State()
+    news = State()
+
+
 class Form(StatesGroup):
     twitter = State()
     vk = State()
