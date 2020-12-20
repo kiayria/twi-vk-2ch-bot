6a109f06fea3b102c3d478d739a30f25f27f2c02
JeRusakow <yegorsr@yandex.ru>
2020-12-20 19:25:23 +0300

    Some more dummy funcs added

--- bot/app/vk/commands.py
+++ bot/app/vk/commands.py
@@ -2,7 +2,7 @@ from app import api, twitter_auth
 from app.vk.utils.keyboards import get_vk_markup
 from app.utils.states import VK_DEFAULT, VK_POST, VK_STATUS
 
-# import vk_api
+import vk_api
 
 
 def vk_menu(update, context):
@@ -40,6 +40,14 @@ def vk_change_status(update, context):
 
 
 def vk_post(update, context):
+    # enter a post text
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(
+        text='Введите статус',
+        reply_markup=get_vk_markup()
+    )
 
     return VK_POST
 
@@ -64,6 +72,16 @@ def process_status(update, context):
 
 
 def process_post(update, context):
+
+    post_text = update.message.text
+    answer_text = 'Запощено'
+
+    vk_api.VkApi(token=hardcode_token).get_api().wall.post(text=post_text)
+
+    update.message.reply_text(
+        answer_text,
+        reply_markup=get_vk_markup()
+    )
 
     return VK_DEFAULT
 
