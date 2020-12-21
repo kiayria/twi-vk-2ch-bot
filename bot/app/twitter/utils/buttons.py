40d0fabae50bf6fcb1d5b1a17b9cf4dabc4fb700
Sibikin Konstantin <sibikin.kv@gmail.com>
2020-12-22 02:19:54 +0300

    Fix. Removed unused code in twitter buttons

--- bot/app/twitter/utils/buttons.py
+++ bot/app/twitter/utils/buttons.py
@@ -1,6 +1,10 @@
-TWITTER_BUTTONS = (
+TWITTER_UNAUTHORIZED_BUTTONS = (
     (('Войти', 'twi_login'),),
-    (('Твитнуть', 'twi_tweet'), ('Лента', 'twi_news')),
+    (('Назад', 'return'),),
+)
+TWITTER_AUTHORIZED_BUTTONS = (
+    (('Твитнуть', 'twi_tweet'),),
+    (('Лента', 'twi_news'),),
     (('Стрим', 'twi_stream'),),
     (('Выйти', 'twi_logout'),),
     (('Назад', 'return'),),
