5708424d2102da3a79f8bc12f003d6cf74d9e614
Sibikin Konstantin <sibikin.kv@gmail.com>
2020-12-21 15:16:35 +0300

    Added example twitter auth

--- auth_api/app.py
+++ auth_api/app.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect
+import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://admin:admin@localhost:27017')
+db = client.test
+collection = db.users
 
 
-@app.route('/')
-def hello():
-    return jsonify(message='Hello')
+@app.route('/twitter')
+def get_verifier():
+    oauth_token_tmp = request.args.get('oauth_token')
+    oauth_verifier = request.args.get('oauth_verifier')
+    res = requests.post(
+        'https://api.twitter.com/oauth/access_token?oauth_token=' + oauth_token_tmp + '&oauth_verifier=' + oauth_verifier)
+    res_split = res.text.split('&')
+    oauth_token = res_split[0].split('=')[1]
+    oauth_secret = res_split[1].split('=')[1]
+    userid = res_split[2].split('=')[1]
+    username = res_split[3].split('=')[1]
+    doc = collection.find_one_and_update(
+        {
+            "oauth_token": oauth_token_tmp
+        },
+        {
+            "$set":
+                {
+                    "oauth_token": oauth_token,
+                    "oauth_token_secret": oauth_secret,
+                    "user_id": userid,
+                    "screen_name": username
+                }
+        }, upsert=True)
+    print(doc)
+    return redirect('http://t.me/twittertgbot')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
